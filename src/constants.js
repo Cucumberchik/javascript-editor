3456b1bd43a54df10772d9c33bd33d7f483e315c
Cucumber Rick <150014886+Cucumberchik@users.noreply.github.com>
2024-05-05 08:54:51 +0600

    change author (contsntants.js)

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const LANGUAGE_VERSIONS = {
 };
 
 export const CODE_SNIPPETS = {
-  javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Umar");\n`,
+  javascript: `\n//Author on Ginhub -"Cucumberchik"\n`,
   typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Umar" });\n`,
   python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Umar")\n`,
   java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
